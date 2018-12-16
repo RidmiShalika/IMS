8c75a633a41dd87a21fd66f19404a22d1ab32676
Ridmi Shalika <Ridmi Shalika@DESKTOP-HV271RC>
2018-12-16 20:13:58 +0530

    2018/12/16

---
+++ src/java/mapping/ExtraClasses.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mapping;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Ridmi Shalika
+ */
+@Entity
+@Table(name = "extra_classes", catalog = "institute_management", schema = "")
+@NamedQueries({
+    @NamedQuery(name = "ExtraClasses.findAll", query = "SELECT e FROM ExtraClasses e")})
+public class ExtraClasses implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Size(max = 30)
+    @Column(name = "date")
+    private String date;
+    @Size(max = 30)
+    @Column(name = "start_time")
+    private String startTime;
+    @Size(max = 30)
+    @Column(name = "end_time")
+    private String endTime;
+    @Size(max = 10)
+    @Column(name = "status")
+    private String status;
+    @JoinColumn(name = "course_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Course courseId;
+
+    public ExtraClasses() {
+    }
+
+    public ExtraClasses(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Course getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Course courseId) {
+        this.courseId = courseId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ExtraClasses)) {
+            return false;
+        }
+        ExtraClasses other = (ExtraClasses) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mapping.ExtraClasses[ id=" + id + " ]";
+    }
+    
+}
